14d01d9d273c86f618de20487f4562032abd8daf
James O'Neill <earjjo@leeds.ac.uk>
2018-03-06 10:17:07 +0000

    Add python script getAQMeshData.py to download AQMesh data using their API

---
+++ Python/getAQMeshData.py
@@ -0,0 +1,102 @@
+from pandas.io.json import json_normalize
+import json
+import requests
+from dateutil.parser import parse
+import pytz
+import argparse
+import datetime as dt
+import os
+
+#####READ IN COMMAND LINE ARGUMENTS AND GET PYTHON DIRECTORY
+#parser = argparse.ArgumentParser()
+#parser.add_argument("startDate",help="Start date/time (UTC) of data to download, in format YYYY-MM-DDTHH:MM:SS, e.g. 2017-01-01T00:00:00",type=str)
+#parser.add_argument("endDate",help="End date/time (UTC) of data to download, in format YYYY-MM-DDTHH:MM:SS, e.g. 2017-31-01T23:59:59",type=str)
+#parser.add_argument("variables",help="List of variables to download, in single quotes separated by spaces, e.g. 'NO PM10 SO2'. Or specify 'ALL'\
+#to download all variables. Full list of available variables: AIRPRES, HUM, NO, NO2, O3, PARTICULE_COUNT, PM1, PM10, PM2.5, PMTOTAL, SO2,\
+#TEMP, VOLTAGE",type=str)
+#args = parser.parse_args()
+#startDate=args.startDate
+#endDate=args.endDate
+#variables=args.variables
+#pyDir = os.path.dirname(os.path.realpath(__file__))
+#
+startDate="2018-01-01T12:00:00"
+endDate="2018-01-05T01:00:00"
+variables='NO PM10 SO2'
+pyDir='/nfs/see-fs-01_users/earjjo/OneDrive/UNRESP/Python/'
+#####
+
+#####PARAMETERS
+allVars=['AIRPRES', 'HUM', 'NO', 'NO2', 'O3', 'PARTICULE_COUNT', 'PM1', 'PM10', 'PM2.5', 'PMTOTAL', 'SO2', 'TEMP', 'VOLTAGE']
+colOrder=['TBTimestamp','TETimestamp','SensorLabel','SensorName','PreScaled','Slope','Offset','Scaled','UnitName','Status']
+#####
+
+#####READ IN ACCOUNT INFO
+codesFile = os.path.join(pyDir,'AQMeshCodes.txt')
+assert os.path.exists(codesFile), "Can't find file AQMeshCodes.txt in same directory as python script"
+f=open(codesFile)
+hdr=f.readline()
+accountID=f.readline().strip()
+licenceKey=f.readline().strip()
+stationID=f.readline().strip()
+f.close()
+#####
+
+#####CHECK VARIABLES
+if variables=='ALL':
+    vars = allVars
+else:
+    vars = [s for s in variables.split()]
+    for v in vars:
+        assert v in allVars, "variable name "+v+" not valid. Full list of available variables: "+str(allVars)
+#####
+
+#####GET VALID TIME RANGE AND CHECK START/END DATES
+try:
+    url = "https://api.airmonitors.net/3.5/GET/"+accountID+"/"+licenceKey+"/stationdata/Period/"+stationID
+    rawText = requests.get(url=url)
+    rawJson = json.loads(rawText.text)
+except:
+    print("Couldn't access data. Are you online? Are the codes in AQMeshCodes.txt correct?")
+    raise
+validStart=parse(rawJson[0]['FirstTETimestamp'])
+validEnd=parse(rawJson[0]['LastTBTimestamp'])
+start=pytz.utc.localize(parse(startDate))
+end=pytz.utc.localize(parse(endDate))
+assert (start-validStart).seconds >+ 0, "The start date/time must come after "+str(validStart)
+assert (validEnd-end).seconds >= 0, "The end date/time must come before "+str(validEnd)
+assert (end-start).seconds >= 0, "The start date/time must come before the end date/time"
+#####
+
+#####SPLIT TIME RANGE INTO DAYS FOR DOWNLOAD
+startDay = dt.datetime(start.year,start.month,start.day,tzinfo=pytz.UTC)
+dateDays = [start]
+nextDay = startDay + dt.timedelta(days=1)
+while nextDay < end:
+    dateDays.append(nextDay)
+    nextDay+=dt.timedelta(days=1)
+dateDays.append(end)
+startDays=dateDays[0:-1]
+endDays=dateDays[1:]
+if(len(endDays)>1):
+    for d,day in enumerate(endDays[:-1]):
+        endDays[d]-=dt.timedelta(seconds=1)
+startDaysStr=[t.strftime('%Y-%m-%dT%H:%M:%S') for t in startDays]
+endDaysStr=[t.strftime('%Y-%m-%dT%H:%M:%S') for t in endDays]
+
+#####LOAD IN DATA
+url = "https://api.airmonitors.net/3.5/GET/"+accountID+"/"+licenceKey+"/stationdata/"+startDate+"/"+endDate+"/"+stationID
+if variables != 'ALL':
+    url=url+"/"+'-'.join(vars)
+rawText = requests.get(url=url)
+rawJson = json.loads(rawText.text)
+rawDF = json_normalize(rawJson,record_path=['Channels'],meta=['TBTimestamp', 'TETimestamp'])
+procDF=rawDF.drop(['Channel'], axis=1)
+procDF=procDF[colOrder]
+procDF=procDF.reindex(index=procDF.index[::-1])
+#parseTE=[parse(t) for t in procDF['TETimestamp']]
+#parseTB=[parse(t) for t in procDF['TBTimestamp']]
+#####
+
+#####SAVE TO CSV
+procDF.to_csv(os.path.join(pyDir,'test.csv'),index=False)
