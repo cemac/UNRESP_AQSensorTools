caa71a8492a45ae0ad7d44ca6bf258ef61c94782
James O'Neill <earjjo@leeds.ac.uk>
2018-03-08 10:17:53 +0000

    Create3DDat.py - Add VVEL, RH and VAPMR to Record 9

--- Python/Create3DDAT.py
+++ Python/Create3DDAT.py
@@ -38,41 +38,41 @@ def writeRec5():
     fout.write(('{:3d}'*23+'\n').format(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 
 def writeRec6():
-    IBYRM=int(startDate[0:4])
-    IBMOM=int(startDate[4:6])
-    IBDYM=int(startDate[6:8])
-    IBHRM=0
-    NHRSMM5=16
+    IBYRM=int(startDate[0:4]) #Beginning year of GRIB data
+    IBMOM=int(startDate[4:6]) #Beginning month of GRIB data
+    IBDYM=int(startDate[6:8]) #Beginning day of GRIB data
+    IBHRM=0 #Beginning hour (GMT) of GRIB data
+    NHRSMM5=nfiles-1 #Length of period (hours of data in file (Replicate Sara's file but should possibly be 3* this value?)
     fout.write(('{:4d}'+'{:02d}'*3+'{:5d}'+'{:4d}'*3+'\n').format(IBYRM,IBMOM,IBDYM,IBHRM,NHRSMM5,NX,NY,NZ))
     
 def writeRec7():
-    NX1=1
-    NY1=1
-    NX2=NX
-    NY2=NY
-    NZ1=1
-    NZ2=NZ
-    RXMIN=lons[iLonMinGRIB]
-    RXMAX=lons[iLonMaxGRIB]
-    RYMIN=lats[iLatMinGRIB]
-    RYMAX=lats[iLatMaxGRIB]
+    NX1=1 #I-index (x-direction) of lower left corner
+    NY1=1 #J-index (y-direction) of lower left corner
+    NX2=NX #I-index (x-direction) of upper right corner
+    NY2=NY #J-index (y-direction) of upper right corner
+    NZ1=1 #K-index of lowest extracted layer
+    NZ2=NZ #K-index of highest extracted layer
+    RXMIN=lons[iLonMinGRIB] #W-most E longitude
+    RXMAX=lons[iLonMaxGRIB] #E-most E longitude
+    RYMIN=lats[iLatMinGRIB] #S-most N latitude
+    RYMAX=lats[iLatMaxGRIB] #N-most N latitude
     fout.write(('{:4d}'*6+'{:10.4f}'*2+'{:9.4f}'*2+'\n').format(NX1,NY1,NX2,NY2,NZ1,NZ2,RXMIN,RXMAX,RYMIN,RYMAX))
-    SIGMA=np.array(levsIncl)/1013.25
+    SIGMA=np.array(levsIncl)/1013.25 #Sigma-p values for each vertical layer (pressure/reference pressure)
     for s in SIGMA:
         fout.write('{:6.3f}\n'.format(s))
 
 def writeRec8():
     IELEVDOT=0 #terrain elevation above MSL (m). Set to zero for now to replicate Sara's file
     ILAND=-9 #Lansuse categories (set to -9 to replicae Sara's file)
-    XLATCRS=-999
-    XLONGCRS=-999
-    IELEVCRS=-999
+    XLATCRS=-999 #Not used
+    XLONGCRS=-999 #Not used
+    IELEVCRS=-999 #Not used
     for j in range(NY):
-        JINDEX=j+1
-        XLATDOT=lats[iLatMinGRIB+j]
+        JINDEX=j+1 #J-index (y-direction) of grid point
+        XLATDOT=lats[iLatMinGRIB+j] #N latitude of grid point
         for i in range(NX): 
-            IINDEX=i+1
-            XLONGDOT=lons[iLonMinGRIB+i]
+            IINDEX=i+1 #I-index (x-direction) of grid point
+            XLONGDOT=lons[iLonMinGRIB+i] #E longitude of grid point
             fout.write(('{:4d}'*2+'{:9.4f}{:10.4f}{:5d}{:3d} {:9.4f}{:10.4f}{:5d}\n').format(IINDEX,JINDEX,XLATDOT,XLONGDOT,IELEVDOT,ILAND,XLATCRS,XLONGCRS,IELEVCRS))
 
 def writeRec9():
@@ -81,23 +81,28 @@ def writeRec9():
     SC=0 #snow cover
     RADSW=0.0 #SW radiation at surface (replicate Sara's file)
     RADLW=0.0 #LW radiation at top (replicate Sara's file)
-    for t in range(17):
+    VAPMR=0.0 #Vagour mixing ratio (replicate Sara's file)
+    for t in range(nfiles):
         print("Processing file "+filenames[t])
         dateTime=parse(startDate)+dt.timedelta(hours=t*3)
         MYR=dateTime.year #Year of data block
         MMO=dateTime.month #Month of data block
         MDAY=dateTime.day #Day of data block
         MHR=dateTime.hour #Hour of data block
-        #####
-        f=open(filePaths[t],'r')
-        gribapi.grib_multi_support_on()
+        #GRIB file processing:
+        f=open(filePaths[t],'r') #Open GRIB file
+        gribapi.grib_multi_support_on() #Turn on multi-message support
         mcount = gribapi.grib_count_in_file(f) #number of messages in file
-        [gribapi.grib_new_from_file(f) for i in range(mcount)]
-        f.close()
+        [gribapi.grib_new_from_file(f) for i in range(mcount)] #Get handles for all messages
+        f.close() #Close GRIB file
+        #Initialse 3D arrays for holding required fields:
         HGTgrd=np.zeros(shape=(Nj,Ni,NZ))
         TMPgrd=np.zeros(shape=(Nj,Ni,NZ))
         Ugrd=np.zeros(shape=(Nj,Ni,NZ))
         Vgrd=np.zeros(shape=(Nj,Ni,NZ))
+        Wgrd=np.zeros(shape=(Nj,Ni,NZ))
+        RHgrd=np.zeros(shape=(Nj,Ni,NZ))
+        #Loop through included levels and store the values in the appropriate k index of the 3D arrays
         for k in range(NZ):
             HGTvals=gribapi.grib_get_values(gidHGT[k])
             HGTgrd[:,:,k]=np.reshape(HGTvals,(Nj,Ni),'C')
@@ -107,14 +112,19 @@ def writeRec9():
             Ugrd[:,:,k]=np.reshape(Uvals,(Nj,Ni),'C')
             Vvals=gribapi.grib_get_values(gidV[k])
             Vgrd[:,:,k]=np.reshape(Vvals,(Nj,Ni),'C')
-        WSgrd=np.sqrt(Ugrd**2+Vgrd**2)
+            Wvals=gribapi.grib_get_values(gidW[k])
+            Wgrd[:,:,k]=np.reshape(Wvals,(Nj,Ni),'C')
+            RHvals=gribapi.grib_get_values(gidRH[k])
+            RHgrd[:,:,k]=np.reshape(RHvals,(Nj,Ni),'C')
+        WSgrd=np.sqrt(Ugrd**2+Vgrd**2) #Calculate wins speed (pythagoras)
+        #Calculate wind direction:
         WDgrd=np.arctan2(Vgrd,Ugrd) #radians, between [-pi,pi], positive anticlockwise from positive x-axis
         WDgrd*=180/np.pi #degrees, between [-180,180], positive anticlockwise from positive x-axis
         WDgrd+=180 #degrees, between [0,360], positive anticlockwise from negative x-axis (Since we specify the direction the wind is blowing FROM, not TO)
         WDgrd=-WDgrd #degrees, between [-360,0], positive clockwise from negative x-axis (Since wind direction is positive clockwise)
         WDgrd+=90 #degrees, between [-270,90], positive clockwise from positive y-axis (Since wind direction is from North)
         WDgrd=np.mod(WDgrd,360) #degrees, between [0,360], positive clockwise from positive y-axis (DONE!)
-        #####
+        #Loop over grid cells:
         for j in range(NY):
             JX=j+1 #J-index of grid cell
             for i in range(NX): 
@@ -126,8 +136,10 @@ def writeRec9():
                     TEMPK=TMPgrd[iLatMinGRIB+j,iLonMinGRIB+i,k] #Temperature (Kelvin)
                     WD=int(WDgrd[iLatMinGRIB+j,iLonMinGRIB+i,k]) #Wind direction (degrees)
                     WS=WSgrd[iLatMinGRIB+j,iLonMinGRIB+i,k] #Wind speed (m/s)
-                    fout.write(('{:4d}{:6d}{:6.1f}{:4d}{:5.1f}\n').format(PRES2,Z,TEMPK,WD,WS))
-        #
+                    W=Wgrd[iLatMinGRIB+j,iLonMinGRIB+i,k] #Vertical velocity (m/s)
+                    RH=int(RHgrd[iLatMinGRIB+j,iLonMinGRIB+i,k]) #Relative humidity (%)
+                    fout.write(('{:4d}{:6d}{:6.1f}{:4d}{:5.1f}{:6.2f}{:3d}{:5.2f}\n').format(PRES2,Z,TEMPK,WD,WS,W,RH,VAPMR))
+        #Release all messages:
         for i in range(mcount):
             gribapi.grib_release(i+1)
                     
@@ -143,14 +155,15 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 #####PARAMETERS
-startDate='20180306'
+startDate='20180306' #Eventually this will be passed in as an argument
 latMinCP=11.7 #Min lat of CALPUFF grid
 latMaxCP=12.2 #Max lat of CALPUFF grid
 lonMinCP=273.2 #Min lon of CALPUFF grid
 lonMaxCP=274.1 #Max lon of CALPUFF grid
-inDir=os.getenv("HOME")+'/UNRESP_ndrive/Data/NAM_20180306'
-outFile=os.getenv("HOME")+'/Data/UNRESP/3D.DAT'
-levsIncl=[1000,950,925,900,850,800,700,600,500,400,300,250,200,150,100,75,50,30,20,10,7,5,2]
+inDir=os.getenv("HOME")+'/UNRESP_ndrive/Data/NAM_20180306' #Parent Directory containing GRIB files
+nfiles=17 #Number of GRIB files (files are 3 hourly, so 48 hours is 17 files including hours 0 and 48)
+outFile=os.getenv("HOME")+'/Data/UNRESP/3D.DAT' #Outpuf ile path
+levsIncl=[1000,950,925,900,850,800,700,600,500,400,300,250,200,150,100,75,50,30,20,10,7,5,2] #pressure levels to include in output
 #####
 
 #####SET FILENAMES
@@ -158,7 +171,7 @@ filePrefix='nam.t00z.afwaca'
 fileSuffix='.tm00.grib2'
 filenames=[]
 filePaths=[]
-for i in range(17):
+for i in range(nfiles):
     filenames.append(filePrefix+'{:02d}'.format(i*3)+fileSuffix)
     filePaths.append(os.path.join(inDir,filenames[i]))
 #####
@@ -171,7 +184,7 @@ gids = [gribapi.grib_new_from_file(f) for i in range(mcount)]
 f.close()
 #####
 
-#####CREATE LIST OF MESSAGE VARIABLE NAMES
+#####GET NAME AND LEVEL OF EACH MESSAGE
 varNames=[]
 levels=[]
 for i in range(mcount):
@@ -180,22 +193,24 @@ for i in range(mcount):
     levels.append(gribapi.grib_get(gid,'level'))
 #####
 
-#####GET REQUIRED GIDS
-gidPRMSL=varNames.index("prmsl")+1
-gidHGT=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'gh' and levels[i] in levsIncl)])
-gidTMP=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 't' and levels[i] in levsIncl)])
-gidU=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'u' and levels[i] in levsIncl)])
-gidV=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'v' and levels[i] in levsIncl)])
+#####GET REQUIRED GIDS (AT REQUIRED LEVELS)
+gidPRMSL=varNames.index("prmsl")+1 #Pressure reduced to mean sea level
+gidHGT=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'gh' and levels[i] in levsIncl)]) #Height
+gidTMP=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 't' and levels[i] in levsIncl)]) #Temperature
+gidU=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'u' and levels[i] in levsIncl)]) #U-component of wind
+gidV=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'v' and levels[i] in levsIncl)]) #V-component of wind
+gidW=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'wz' and levels[i] in levsIncl)]) #W-component of wind
+gidRH=np.flipud([i+1 for i in range(len(varNames)) if (varNames[i] == 'r' and levels[i] in levsIncl)]) #Relative humidity
 #####
 
-#####GET LATS, LONS, ETC
+#####GET LATS, LONS, NI AND NJ
 lats=gribapi.grib_get_array(gidPRMSL,'distinctLatitudes')
 lons=gribapi.grib_get_array(gidPRMSL,'distinctLongitudes')
 Ni=gribapi.grib_get(gidPRMSL,'Ni')
 Nj=gribapi.grib_get(gidPRMSL,'Nj')
 #####
 
-#####DETERMINE SUBDOMAIN INDICES
+#####DETERMINE SUBDOMAIN INDICES BASED ON CALPUFF GRID EXTENT
 for i in range(len(lats)-1):
     if lats[i+1] >= latMinCP:
         iLatMinGRIB=i
@@ -220,7 +235,7 @@ NY=iLatMaxGRIB-iLatMinGRIB+1 #NY, i.e. number of latitudes in GRIB subset grid
 NZ=len(levsIncl) #NZ, i.e. number of levels to be extracted from GRIB subset grid
 #####
 
-###PLOT A MESSAGE
+#####PLOT A MESSAGE
 #gidPlot=gidHGT[-2]
 #Ni=gribapi.grib_get(gidPlot,'Ni')
 #Nj=gribapi.grib_get(gidPlot,'Nj')
@@ -260,22 +275,3 @@ writeRec9()
 #####CLOSE OUTPUT FILE
 fout.close()
 #####
-
-
-
-
-
-##Get MSLP field
-#iterid=gribapi.grib_iterator_new(gidPRMSL,0)
-#missingValue=gribapi.grib_get_double(gidPRMSL,"missingValue")
-#while 1:
-#    result=gribapi.grib_iterator_next(iterid)
-#    if not result: break
-#    [lat,lon,value] = result 
-#    if latMin < lat < latMax and lonMin < lon < lonMax:
-#        if value == missingValue:
-#            print("missing")
-#        else:
-#            print("lat=%.3f, lon=%.3f, value=%.3f" % (lat,lon,value))
-#gribapi.grib_iterator_delete(iterid)
-
