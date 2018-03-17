d157a4d81dc1c82699df1426783bf333c6a905fe
James O'Neill <earjjo@leeds.ac.uk>
2018-03-17 23:56:06 +0000

    If 2nd day of new month, re-download full previous month

--- Python/getLatestAQMeshData.py
+++ Python/getLatestAQMeshData.py
@@ -17,9 +17,16 @@ startOfYdayStr=startOfYday.strftime('%Y-%m-%dT%H:%M:%S')
 #Download data up to most recent time:
 getAQMeshData.main('1733150',startOfYdayStr,'end','ALL','all')
 
-#If into new day, download full day from 2 days ago
+#If into new day, download full day from 2 days ago:
 if (now-dt.timedelta(hours=1)).day != now.day:
     startOf2DaysAgo=dt.datetime(now.year,now.month,now.day)-dt.timedelta(days=2)
     startOf2DaysAgoStr=startOf2DaysAgo.strftime('%Y-%m-%dT%H:%M:%S')
     getAQMeshData.main('1733150',startOf2DaysAgoStr,startOfYdayStr,'ALL','daily')
     
+#If into 2nd day of new month, download full previous month:
+if (now-dt.timedelta(days=1,hours=1)).month != now.month:
+    startOfLastMonth=dt.datetime(startOf2DaysAgo.year,startOf2DaysAgo.month,1)
+    startOfLastMonthStr=startOfLastMonth.strftime('%Y-%m-%dT%H:%M:%S')
+    startOfThisMonth=dt.datetime(now.year,now.month,1)
+    startOfThisMonthStr=startOfThisMonth.strftime('%Y-%m-%dT%H:%M:%S')
+    getAQMeshData.main('1733150',startOfLastMonthStr,startOfThisMonthStr,'ALL','monthly')
