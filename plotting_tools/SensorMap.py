6dd9b068432f115fe110cacef356e973cecbdf4d
hlburns <H.L.Burns@leeds.ac.uk>
2019-01-16 11:54:19 +0000

    :hammer_and_wrench: setting up plotting functions

--- plotting_tools/SensorMap.py
+++ plotting_tools/SensorMap.py
@@ -10,7 +10,7 @@ Example:
 
 Attributes:
 
-.. CEMAC_stomtracking:
+.. CEMAC_AQMesh:
    https://github.com/cemac/UNRESP_AQSensorTools
 """
 
@@ -20,22 +20,50 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 from pytz import timezone
 from datetime import datetime
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from cartopy.io import shapereader
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from matplotlib.font_manager import FontProperties
+import cartopy.io.img_tiles as cimgt
 
-SName = ["SanJu1", "ElCrucero", "SanJuan2", "785150", "Pacaya", "Rigoberto",
-         "861150", "Met", "ElPanama"]
-latMin = 11.7
-latMax = 12.2
-lonMin = -86.7
-lonMax = -86.0
-bmap = Basemap(llcrnrlon=lonMin, llcrnrlat=latMin, urcrnrlon=lonMax, urcrnrlat=latMax)
-esri_url = \
-    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/export?\
-bbox=%s,%s,%s,%s&\
-bboxSR=%s&\
-imageSR=%s&\
-size=%s,%s&\
-dpi=%s&\
-format=png32&\
-f=image" %\
-(bmap.llcrnrlon, bmap.llcrnrlat, bmap.urcrnrlon, bmap.urcrnrlat, bmap.epsg, bmap.epsg, xpixels, bmap.aspect*xpixels, 96)
-ESRIimg = mpimg.imread(esri_url)
+
+# Station lat lonts
+fname = '../aqtools/information.csv'
+data = pd.read_csv(fname)
+
+Stations = data.StationName
+
+towns = (' El Panama', ' Rigoberto', ' Pacaya', ' El Crucero',
+         ' La Concepcion', ' Masaya', ' San Marcos', ' Jinotepe')
+townCoords = ((-86.2058, 11.972), (-86.2021, 11.9617), (-86.3013, 11.9553),
+              (-86.3113, 11.9923), (-86.189772, 11.936161),
+              (-86.096053, 11.973523), (-86.20317, 11.906584),
+              (-86.19993, 11.85017))
+volcCoords = (-86.1608, 11.9854)
+
+
+font = FontProperties()
+font.set_weight('bold')
+font.set_family('monospace')
+
+
+extent = [-86.7, -86.0, 11.7, 12.2]
+request = cimgt.OSM()
+
+fig = plt.figure(figsize=(9, 13))
+ax = plt.axes(projection=request.crs)
+gl = ax.gridlines(draw_labels=True, alpha=0.2)
+gl.xlabels_top = gl.ylabels_right = False
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+
+# ax.set_extent(extent)
+ax.set_extent(extent)
+ax.add_image(request, 10, interpolation='spline36')
+for i, town in enumerate(towns):
+    ax.plot(townCoords[i][0], townCoords[i]
+                         [1], 'og', markersize=4, transform=ccrs.Geodetic())
+ax.plot(volcCoords[0], volcCoords[1], '^r',
+        markersize=6, transform=ccrs.Geodetic())
+plt.show()
--- plotting_tools/Sensor_plots.py
+++ plotting_tools/Sensor_plots.py
@@ -23,130 +23,63 @@ from datetime import datetime
 
 SName = ["SanJu1", "ElCrucero", "SanJuan2", "785150", "Pacaya", "Rigoberto",
          "861150", "Met", "ElPanama"]
-myFmt = DateFormatter("%b")
-for StationName in SName:
-    # Format the data
-    #try:
-    #    allSO2 = pd.read_csv('../data/' + StationName + '/allSO2.csv')
-    #    allNO2 = pd.read_csv('../data/' + StationName + '/allNO2.csv')
-    #except FileNotFoundError:
-    example = pd.read_csv('../data/ElPanama/AQMeshData_1733150_2018-01-01T00' +
-                          '-00-00_to_2018-12-03T09-59-10_SO2-NO2.csv')
-    pname = '../data/' + StationName + '/AQMeshData*.csv'
+myFmt = DateFormatter("%Y %b ")
+myFmtday = DateFormatter("%d ")
+
+
+def plotTSNO2(StationName, month=1, year=2016, ymax=100):
+    pname = '../data/' + StationName + '/AQMeshData*_2016*.csv'
     for rw in glob.iglob(pname):
         day_data = pd.read_csv(rw)
         NO2 = day_data[day_data.SensorLabel == 'NO2']
-        SO2 = day_data[day_data.SensorLabel != 'NO2']
+        SO2 = day_data[day_data.SensorLabel == 'SO2']
         NO2_valid = NO2[NO2.Status == 'Valid']
         SO2_valid = SO2[SO2.Status == 'Valid']
-        allSO2 = SO2_valid.reset_index(drop=True)
         allNO2 = NO2_valid.reset_index(drop=True)
-
+        allSO2 = SO2_valid.reset_index(drop=True)
     allNO2['TBTimestamp'] = pd.to_datetime(allNO2['TBTimestamp'])
-    allSO2['TBTimestamp'] = pd.to_datetime(allSO2['TBTimestamp'])
     allNO2['TBTimestamp'].apply(lambda x: datetime.replace(x, tzinfo=None))
-    allSO2['TBTimestamp'].apply(lambda x: datetime.replace(x, tzinfo=None))
     allNO2['date'] = allNO2['TBTimestamp'].apply(lambda x: x.strftime("%Y %b"))
+    allSO2['TBTimestamp'] = pd.to_datetime(allNO2['TBTimestamp'])
+    allSO2['TBTimestamp'].apply(lambda x: datetime.replace(x, tzinfo=None))
     allSO2['date'] = allSO2['TBTimestamp'].apply(lambda x: x.strftime("%Y %b"))
-    print('../data/' + StationName + '/allSO2.csv')
-    allSO2.to_csv('../data/' + StationName + '/allSO2.csv', sep=',')
-    allNO2.to_csv('../data/' + StationName + '/allNO2.csv', sep=',')
-
-    # plot whole dataset
-    T1 = datetime(2018, 1, 1, 0, 0, 0).replace(tzinfo=None)
-    T2 = datetime(2018, 12, 1, 0, 0, 0).replace(tzinfo=None)
-    plt.figure(figsize=(100, 8))
+    T1 = datetime(year, month, 1, 0, 0, 0).replace(tzinfo=None)
+    if month == 12:
+        T2 = datetime(year, month, 31, 0, 0, 0).replace(tzinfo=None)
+    else:
+        T2 = datetime(year, month + 1, 1, 0, 0, 0).replace(tzinfo=None)
+    plt.figure(figsize=(30, 10))
     ax = plt.gca()
-    ax.set_title(r'NO$_2$ 2018', fontsize=48)
+    ax.set_title(StationName + r' NO$_2$ and SO$_2$' + '\n', fontsize=48)
     if not allNO2.empty:
-        allNO2.plot.line(x='TBTimestamp', y='Scaled', ax=ax)
+        allNO2.plot.line(x='TBTimestamp', y='Scaled', ax=ax, color='b')
         ax.set_xlim([T1, T2])
+        ax.set_ylabel('NO$_2$', fontsize=48, color='b')
         ax.tick_params(axis='both', which='major', labelsize=48)
-        ax.xaxis.set_major_formatter(myFmt)
+        ax.tick_params(axis='y', which='major', labelsize=48, colors='b')
+        ax.xaxis.set_major_formatter(myFmtday)
         ax.xaxis_date()
-        plt.savefig('../plots/' + StationName + '/NO22018.png')
-        plt.figure(figsize=(100, 8))
-        ax = plt.gca()
-    if allSO2.empty:
-        continue
-    ax.set_title(r'SO$_2$ 2018', fontsize=48)
-    allSO2.plot.line(x='TBTimestamp', y='Scaled', color='red', ax=ax)
-    ax.set_xlim([T1, T2])
-    ax.tick_params(axis='both', which='major', labelsize=48)
-    ax.xaxis.set_major_formatter(myFmt)
-    ax.xaxis_date()
-    plt.savefig('../plots/' + StationName + '/SO22018.png')
+        ax.set_xlabel(T1.strftime('%b %Y'), fontsize=48)
+        ax2 = ax.twinx()
+        allSO2.plot.line(x='TBTimestamp', y='Scaled', ax=ax2, color='g')
+        ax2.set_xlim([T1, T2])
+        ax2.xaxis_date()
+        ax2.xaxis.set_major_formatter(myFmtday)
+        ax2.tick_params(axis='y', which='major', labelsize=48, colors='g')
+        ax2.set_ylabel('SO$_2$', fontsize=48, color='g')
+        plt.tight_layout()
+        ax2.get_legend().remove()
+        ax.get_legend().remove()
+        plt.savefig('../plots/' + str(StationName) + '/' +
+                    str(T1.strftime('%b_%Y')) + '.png')
+    return
 
 
-def plotterNO2(time_p, T1, T2):
-    plt.figure(figsize=(15, 8))
-    mask = ((allNO2['TBTimestamp'] > T1) &
-            (allNO2['TBTimestamp'] <= T2))
-    TPNO2 = allNO2.loc[mask]
-    ax = plt.gca()
-    ax.set_title('NO$_{2}$ ' + time_p + ' 2018', fontsize=24)
-    ax.set_ylabel(' (ppb) ', fontsize=20)
-    TPNO2.plot.line(x='TBTimestamp', y='Scaled', ax=ax)
-    ax.tick_params(axis='both', which='major', labelsize=18)
-    ax.legend(['NO$_2$'], fontsize=18)
-    # Remove axis label
-    ax1 = plt.axes()
-    ax1.xaxis.label.set_visible(False)
-    plt.savefig('../plots/' + StationName + '/NO2' + time_p + '.png')
-
-
-def plotterSO2(time_p, T1, T2):
-    plt.figure(figsize=(15, 8))
-    mask = ((allSO2['TBTimestamp'] > T1) &
-            (allSO2['TBTimestamp'] <= T2))
-    TPSO2 = allSO2.loc[mask]
-    ax = plt.gca()
-    ax.set_title('SO$_{2}$ ' + time_p + ' 2018', fontsize=24)
-    ax.set_ylabel(' (ppb) ', fontsize=20)
-    TPSO2.plot.line(x='TBTimestamp', y='Scaled', ax=ax)
-    ax.tick_params(axis='both', which='major', labelsize=18)
-    ax.legend(['SO$_2$'], fontsize=18)
-    # Remove axis label
-    ax1 = plt.axes()
-    ax1.xaxis.label.set_visible(False)
-    plt.savefig('../plots/' + StationName + '/SO2' + time_p + '.png')
-
-
-# El Panama
-StationName = 'ElPanama'
-allSO2 = pd.read_csv('../data/' + StationName + '/allSO2.csv')
-allNO2 = pd.read_csv('../data/' + StationName + '/allNO2.csv')
-T1s = ['2018-01-01 00:00:00', '2018-04-01 00:00:00',
-       '2018-08-01 00:00:00', '2018-10-18 00:00:00']
-T2s = ['2018-04-01 00:00:00', '2018-06-10 00:00:00',
-       '2018-10-12 00:00:00', '2018-11-20 00:00:00']
-
-TP = ['Jan2Apr', 'Apr2Jun', 'Jul2Oct', 'Recent']
-
-for i in range(len(T1s)):
-    plotterNO2(TP[i], T1s[i], T2s[i])
-    plotterSO2(TP[i], T1s[i], T2s[i])
-
-# SanJuan
-StationName = 'SanJuan'
-allSO2 = pd.read_csv('../data/' + StationName + '/allSO2.csv')
-allNO2 = pd.read_csv('../data/' + StationName + '/allNO2.csv')
-plotterNO2('Jan2Feb', '2018-01-01 00:00:00', '2018-02-01 00:00:00')
-plotterSO2('Jan2Feb', '2018-01-01 00:00:00', '2018-02-01 00:00:00')
-
-# StationA
-StationName = 'StationA'
-allSO2 = pd.read_csv('../data/' + StationName + '/allSO2.csv')
-allNO2 = pd.read_csv('../data/' + StationName + '/allNO2.csv')
-plotterNO2('Aug', '2018-07-01 00:00:00', '2018-09-01 00:00:00')
-plotterSO2('Aug', '2018-07-01 00:00:00', '2018-09-01 00:00:00')
-
-# StationB
-StationName = 'StationB'
-allSO2 = pd.read_csv('../data/' + StationName + '/allSO2.csv')
-allNO2 = pd.read_csv('../data/' + StationName + '/allNO2.csv')
-plotterNO2('Aug', '2018-07-01 00:00:00', '2018-09-01 00:00:00')
-plotterSO2('Aug', '2018-07-01 00:00:00', '2018-09-01 00:00:00')
+# To create plots:
+years = [2016, 2017, 2018]
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for m in months:
+    plotTSNO2(SName[2], month=months[m - 1], year=years[0])
 
 # bin the data
 binLims = [0, 10, 350, 600, 2600, 9000, 14000]
@@ -176,6 +109,8 @@ def plotterSO2_binned(time_p, T1, T2):
     # Remove axis label
     ax1 = plt.axes()
     ax1.xaxis.label.set_visible(False)
+
+
 """
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import show, figure, output_file
