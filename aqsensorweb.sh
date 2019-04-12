0c73eb48ccd1680da866dc344ab22bc40ef899fb
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-12 12:40:11 +0100

    :sparkles: started a script for automating TS web update

---
+++ aqsensorweb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :aqsensorweb.sh
+#description    :Grab data for air quality sensors (all) and plot a monthly timeseries and move images to the web framework set up in the UNRESP forcasting system
+#author         :CEMAC - helen
+#date           :20190411
+#version        :1.0    
+#usage          :./aqsensorweb.sh
+#notes          :       
+#bash_version   :4.2.46(2)-release
+#============================================================================
+
+
